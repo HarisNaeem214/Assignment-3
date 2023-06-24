49428e88e2e5b7f4dd28a7d4041889d5ee9dd165
HarisNaeem214 <Harisnaeem214@gmail.com>
2023-06-24 07:37:48 -0700

    Assignment.3

--- q13.dart
+++ q13.dart
@@ -1,6 +1,10 @@
 void main() {
+  /*Implement a code that takes in a list of integers and
+  returns a new list containing only the unique elements
+   from the original list. The order of elements in the 
+  new list should be the same as in the original list.
+*/
   List<int> num = [1, 2, 3, 4, 2, 3, 1, 5];
-
-  var unique = num.removedu()
-
+  num.removeWhere((num) => num == r.int);
+  print(num.append);
 }
--- q6.dart
+++ q6.dart
@@ -1,3 +1,26 @@
-voidmain(){
-  
+void main() {
+  /*  Create Map variable name world then inside it create countries Map,
+   Key will be the name country & country value will have another map having
+    capitalCity, currency and language to it. by using any country key print 
+    all the value of Capital & Currency.*/
+
+  Map World = {
+    'Pakistan': {
+      'capitalCity': "Karachi",
+      'currency': "PakRupees",
+      "Language": "Urdu"
+    },
+    "India": {
+      'capitalCity': "Mumbai",
+      'currency': "IndRupees",
+      "Language": "Not any Lanuage"
+    },
+    "China": {
+      'capitalCity': "Beijing",
+      'currency': "Yuan",
+      "Language": "Mandarin"
+    }
+  };
+
+ print(World['Pakistan']);
 }
